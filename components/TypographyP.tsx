23726fc119b7b046a5da895e324330b5d64149f4
Jonas Broms <jonas@odduse.com>
2024-02-27 00:37:05 +0200

    feat: :boom: install tailwindui commit template

--- components/TypographyP.tsx
+++ components/TypographyP.tsx
@@ -1,7 +1,9 @@
-import React, { FC, ReactNode } from 'react';
+import React, { ReactNode, ReactElement } from 'react';
 
-const TypographyP: FC<{ children: ReactNode }> = ({ children }) => {
-	return <p className="mt-2 font-normal lg:text-2xl text-base bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-		{children}
-	</p>;
+export const TypographyP = ({ children }: { children: ReactNode; }): ReactElement => {
+	return (
+		<p className="mt-2 font-normal lg:text-2xl text-base bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+			{children}
+		</p>
+	);
 }
